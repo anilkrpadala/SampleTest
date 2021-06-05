6644e13e3671bcbd4b9f15d0709bf218e4460408
Anil <Anilkrpadala@gmail.com>
2021-06-05 01:10:10 -0400

    1st commit

---
+++ SampleTest/Class1.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        public IWebDriver driver;
+        [SetUp]
+        public void setup()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--start-maximized");
+            chromeOptions.AddArguments("--window-size=1920,1080");
+            chromeOptions.AddArguments("headless");
+        }
+        [Test]
+        public void DemoReportPass()
+        {
+            Assert.IsTrue(true);
+        }
+        [Test]
+        public void DemoReportFail()
+        {
+            Assert.IsTrue(false);
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
